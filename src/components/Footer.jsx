e51e243b527b02a2d022e7a55f163429858b730c
Amenbreaker <resident_e4@hotmail.com>
2024-09-18 18:43:28 -0300

    Colores + intento footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return(
+        <div className = 'absolute bottom-0 px-4 mx-auto text-white'>
+            <ul className = 'flex'>
+                <li className='p-2 text-[#CBDF00]'>LOGO</li>
+                <li className='p-2'>ALGO</li>
+                <li className='p-2'>ALGO</li>
+            </ul>
+        </div>
+    )
+  };
+
+export default Footer
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
     return(
-        <div className = 'text-white'>
-            <h1 className='w-full text-3xl font-bold text-[#000000]'>ALGO</h1>
+        <div className = 'absolute top-0 px-4 mx-auto text-white'>
+            <h1 className='w-full text-3xl font-bold text-[#CBDF00]'>NAVBAR TEST</h1>
         </div>
     )
 }
